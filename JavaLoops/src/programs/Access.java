b99bf7c6b5267ab6d38cb61c5f5d0a544bed7c16
Vivek Duvvuru <vivekduv@gmail.com>
2024-02-10 22:53:40 -0500

    commit 3 Access.java

--- JavaLoops/src/programs/Access.java
+++ JavaLoops/src/programs/Access.java
@@ -3,7 +3,7 @@ package programs;
 public class Access {
 
 	public static void main(String[] args) {
-	System.out.print("Let us do right");
+	System.out.print("Let us do thia thing right");
 		
 
 	}
