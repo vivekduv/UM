79cb47ac19c12acfd9206c6f517987212fae8855
vivek <vivekduv@gmail.com>
2024-02-11 15:45:22 -0500

    Access v1

--- JavaLoops/src/programs/Access.java
+++ JavaLoops/src/programs/Access.java
@@ -1,16 +1,47 @@
 package programs;
 
+import java.util.Scanner;
+
 public class Access {
-public static void main(String[] args) {
 
-		int counter = 10;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-		do {
-			System.out.println("Count is: " + counter);
-			counter++;
-		} while (counter <= 10);
+		String passwordInput = "";
+		int numInput;
+		int counter = 1;
+		boolean loopAgain = true;
+		boolean quit = false;
+
+		while (!quit && loopAgain && counter++ <= 3) {
+
+			System.out.print("Enter password: ");
+			passwordInput = scanner.next();
+
+			if (!passwordInput.equals("quit")) {
+
+				System.out.print("Enter number: ");
+				numInput = scanner.nextInt();
+
+				if (passwordInput.equals("terps") && numInput == 1847) {
+					System.out.println("Access Granted");
+					loopAgain = false;
+
+				} else {
+					System.out.println("Wrong credentials");
+					loopAgain = true;
+
+				}
+
+			} else {
+				quit = true;
+			}
+		}
+		if (loopAgain && quit) {
+			System.out.println("Access Denied");
+
+		}
+		scanner.close();
 	}
-	// TODO Auto-generated method stub
-
 
 }
